e655171e8b2316a901b437870a7e1f8fcb5f1f63
Bruna Garcia de Almeida <87019102+LipeNebrosa@users.noreply.github.com>
2024-07-11 18:13:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function maiorMenor() {
+    var maiorMenor = document.getElementById("maiorMenor");
+    var maior = 0;
+    var menor = -Infinity;
+    for (var cont = 0; cont < 20; cont++) {
+        var numero = parseInt(prompt("Digite um valor"));
+        if (numero > maior) {
+            maior = numero;
+        }
+        else if (numero < menor) {
+            menor = numero;
+        }
+    }
+    maiorMenor.innerHTML = "O maior número digitado é: " + maior + ". <br/>" +
+        "O menor número digitado é: " + menor + ". <br/>";
+}
+function impares() {
+    var impar = document.getElementById("impar");
+    var html = '';
+    for (i = 150; i <= 300; i++) {
+        if (i % 2 != 0) {
+            html += i + "; ";
+        }
+        else { continue; }
+    }
+    impar.innerHTML = html;
+}
+function mediaTurma() {
+    var mediasPonderadas = [];
+    for(var aluno = 1; aluno <= 4; aluno++) {
+        var nota1 = 
+            (prompt(`Digite a primeira nota do aluno ${aluno}: `));
+        var nota2 = parseFloat(prompt(`Digite a segunda nota do aluno ${aluno}: `));
+        var nota3 = parseFloat(prompt(`Digite a terceira nota do aluno ${aluno}: `));
+        if(!isNaN(nota1) && !isNaN(nota2) && !isNaN(nota3)){
+            var mp = (nota1*2 + nota2*4 + nota3*3) / 9;
+            mediasPonderadas.push(mp);  
+            document.write(`Média ponderada do aluno ${aluno}: ${mp.toFixed(2)} <br/>`);
+        } else {
+            document.write(`Notas inválidas para o aluno ${aluno}.`);
+        }
+    }
+    if(mediasPonderadas.length > 0) {
+        var somaMedias = mediasPonderadas.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+        var mediaGeral = somaMedias / mediasPonderadas.length;
+        document.write(`\n <h4> Média geral da turma: ${mediaGeral.toFixed(2)} </h4>`);
+    } else {
+        document.write(`Não deu pra calcular a média da turma :c `);
+    }
+}
